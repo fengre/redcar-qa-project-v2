d0b5ffcc7dcb133d530b45fd42b925f176603672
Connie Xu <clx@princeton.edu>
2025-06-20 21:33:47 -0500

    fix api key issue

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../.env', // Point to the root .env file
+      isGlobal: true,
+    }),
+    HttpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
